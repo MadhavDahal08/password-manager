7bdbb248ba4b7f3c130b0205317561ce6e587d94
Madhav Dahal <madhavdahal4@gmail.com>
2024-05-01 13:40:59 +0545

    Add search button option to search email and password

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, shuffle, choice
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
 def password_generator():
 #Password Generator Project
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -27,29 +27,53 @@ def password_generator():
     password_entry.insert(0, random_password)
     pyperclip.copy(random_password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
 def store_credentials():
-
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
 
-        is_ok = messagebox.askokcancel(title=f"{website}", message=f"These are the entered:\nEmail: {email}\nPassword: {password}\nIt is ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
             website_entry.focus()
 
+#------------------------------SEARCH CREDENTIALS-------------------------#
+
+def search_credentials():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            user_search = website_entry.get()
+            user= data[user_search]
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    except KeyError:
+        messagebox.showinfo(title="Error", message=f"No details of the {user_search} exists")
+    else:
+        messagebox.showinfo(title=f"{user_search}", message=f"Email: {user["email"]}\nPassword: {user["password"]}")
 
 
-
-# ---------------------------- UI SETUP ------------------------------- #
+#---------------------------- UI SETUP ------------------------------- #
 #Creating tkinter window
 window = Tk()
 window.title("Password Manager")
@@ -82,7 +106,7 @@ email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "madhavdahal4@gmail.com")
 
 password_entry = Entry(width=22)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=3, column=1,)
 
 #Buttons
 generate_password = Button(text="Generate Password", command=password_generator)
@@ -92,7 +116,8 @@ add = Button(text="Add", width=34, command=store_credentials)
 add.grid(row=4, column=1, columnspan=2)
 
 
-
+search = Button(text="Search", command=search_credentials)
+search.grid(row=1, column=2)
 
 
 
